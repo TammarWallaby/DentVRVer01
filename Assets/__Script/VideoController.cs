3e1b226498c9680283bbcea099de0af5a2c0438c
52jjoee <mg4725@naver.com>
2024-12-02 13:02:03 +0900

    11
    11

--- Assets/__Script/VideoController.cs
+++ Assets/__Script/VideoController.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Video;
+using System.Collections;
 
 public class VideoController : MonoBehaviour
 {
     public VideoPlayer videoPlayer;  // Video Player 참조
     public Button playButton;        // 재생 버튼
+    public Image playImage;
+    public Image pauseImage;
+
+
 
     void Start()
     {
@@ -19,13 +24,23 @@ public class VideoController : MonoBehaviour
         if (videoPlayer != null && !videoPlayer.isPlaying)
         {
             videoPlayer.Play();
+            playImage.gameObject.SetActive(true);
+            pauseImage.gameObject.SetActive(false);
+            StartCoroutine(HidePlayImageAfterDelay());
         }
 
         if (videoPlayer != null && videoPlayer.isPlaying)
         {
             videoPlayer.Pause();
+            playImage.gameObject.SetActive(false);
+            pauseImage.gameObject.SetActive(true);
         }
+
     }
-
-
+    private IEnumerator HidePlayImageAfterDelay()
+    {
+        yield return new WaitForSeconds(1f);  // 1초 대기
+        playImage.gameObject.SetActive(false);  // playImage 비활성화
+    }
 }
+
