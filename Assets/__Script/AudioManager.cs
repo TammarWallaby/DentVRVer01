74618152c36e00042075f9c2cfe4054141c688ce
goomo69 <pz01052711901@gmail.com>
2024-12-06 11:49:15 +0900

    1

--- Assets/__Script/AudioManager.cs
+++ Assets/__Script/AudioManager.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+<<<<<<< Updated upstream
     private AudioSource myAudio;
+=======
+    AudioSource myAudio;
+>>>>>>> Stashed changes
     // Start is called before the first frame update
     void Start()
     {
         myAudio = GetComponent<AudioSource>();
+<<<<<<< Updated upstream
         myAudio.Play();
+=======
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
         
+=======
+        myAudio.Play();
+>>>>>>> Stashed changes
     }
 }
