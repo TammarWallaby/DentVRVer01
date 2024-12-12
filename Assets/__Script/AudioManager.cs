824b322215a73170bdb02f5ea0118bef942e968f
goomo69 <pz01052711901@gmail.com>
2024-12-12 15:22:02 +0900

    AudioManager
    Audioclip Insert

--- Assets/__Script/AudioManager.cs
+++ Assets/__Script/AudioManager.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
     AudioSource[] myAudio;
+    public SpriteRenderer[] mySR_S;
+    public Text UI;
     // Start is called before the first frame update
     void Start()
     {
         myAudio = GetComponent<AudioSource[]>();
+   
     }
 
     // Update is called once per frame
