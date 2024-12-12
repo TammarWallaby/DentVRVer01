6b2c91a9ad1c6baab9486c0529c5ce091d0df90d
goomo69 <pz01052711901@gmail.com>
2024-12-12 15:38:41 +0900

    Amending
    Amending a partial part

--- Assets/__Script/AudioManager.cs
+++ Assets/__Script/AudioManager.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 public class AudioManager : MonoBehaviour
 {
     AudioSource[] myAudio;
-    public SpriteRenderer[] mySR_S;
     public Text UI;
     // Start is called before the first frame update
     void Start()
