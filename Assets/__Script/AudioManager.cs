377562bd187be3f038831fa6c9b996972884af0b
goomo69 <pz01052711901@gmail.com>
2024-12-12 17:23:44 +0900

    script amending
    AudioManager script arrange experession change

--- Assets/__Script/AudioManager.cs
+++ Assets/__Script/AudioManager.cs
@@ -10,7 +10,7 @@ public class AudioManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        myAudio = GetComponent<AudioSource[]>();
+        myAudio = GetComponents<AudioSource>();
    
     }
 
